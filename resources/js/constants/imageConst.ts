66153bce43b36b3042c979b718043a79d1335fd2
luannt <nguyenthanhluanbntsbd@gmail.com>
2025-10-08 06:21:25 +0000

    feat: init project

--- resources/js/constants/imageConst.ts
+++ resources/js/constants/imageConst.ts
@@ -1,11 +1,6 @@
-import logoBlack from '@/assets/img/logo_black.png';
-import logoWhite from '@/assets/img/logo_white.png';
 import viFlag from '@/assets/img/flags/vi.png';
 import enFlag from '@/assets/img/flags/en.png';
 import jaFlag from '@/assets/img/flags/ja.png';
-
-export const LOGO_BLACK = logoBlack;
-export const LOGO_WHITE = logoWhite;
 
 export const VI_FLAG = viFlag;
 export const EN_FLAG = enFlag;
--- resources/js/utils/load-locale-messages.ts
+++ resources/js/utils/load-locale-messages.ts
@@ -1,27 +1,35 @@
-import { I18n } from 'vue-i18n';
-import { handleError } from '@/utils/handle-error';
-import { LocaleMessage } from '@/types/common';
-import { SUPPORTED_LOCALES, SUPPORTED_MODULES } from '@/constants/i18n';
-import { $t } from './i18n';
+import { I18n } from 'vue-i18n'
+import { handleError } from '@/utils/handle-error'
+import { LocaleMessage } from '@/types/common'
+import { SUPPORTED_LOCALES, SUPPORTED_MODULES } from '@/constants/i18n'
+import { $t } from './i18n'
 
 export const loadLocaleMessages = async (locale: string, i18n: I18n) => {
-  if (!SUPPORTED_LOCALES.includes(locale)) return;
+  if (!SUPPORTED_LOCALES.includes(locale)) return
 
-  const messages: Record<string, LocaleMessage> = {};
+  const messages: Record<string, LocaleMessage> = {}
+  const allLocaleFiles = import.meta.glob('@/locales/**/*.json', { eager: true })
 
   for (const mod of SUPPORTED_MODULES) {
     try {
-      const msgModule = await import(`@/locales/${locale}/${mod}.json`);
-      messages[mod] = msgModule.default;
+      const matchKey = Object.keys(allLocaleFiles).find((path) =>
+        path.includes(`/${locale}/${mod}.json`)
+      )
+
+      if (matchKey) {
+        messages[mod] = (allLocaleFiles[matchKey] as any).default
+      } else {
+        console.warn(`${$t('common.missingLocale')}: ${locale}/${mod}.json`)
+      }
     } catch (error) {
-      console.error(error);
+      console.error(error)
       handleError('', {
         title: $t('common.loadLanguageError'),
-      });
+      })
     }
   }
 
   if (Object.keys(messages).length > 0) {
-    i18n.global.setLocaleMessage(locale, messages);
+    i18n.global.setLocaleMessage(locale, messages)
   }
-};
+}
